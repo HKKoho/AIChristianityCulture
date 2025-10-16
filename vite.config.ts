1703bf9dc32e266827bad2ddfd9bcbe76c03bafe
HKKoho <160508624+HKKoho@users.noreply.github.com>
2025-10-16 20:19:27 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        open: true,
+        allowedHosts: true,
         proxy: {
           '/api': {
             target: 'http://localhost:3001',
